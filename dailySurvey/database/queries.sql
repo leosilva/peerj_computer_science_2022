cf50f55b73467f61cbe5f5084a816adee26bcac6
Leo Moreira Silva <leo.moreira@me.com>
2022-01-05 21:17:19 -0300

    Creating queries.sql

---
+++ dailySurvey/database/queries.sql
@@ -0,0 +1,25 @@
+use dailyonlinesurvey;
+
+select * from User;
+select * from Survey;
+
+insert into User (id_str_twitter, participant_id, name, screen_name, email)
+VALUES (123456789,99,'Leo Silva','oleomsilva', 'leopontosilva@gmail.com');
+
+
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (23781146,1,'Fe Rebelatto','rebelatto', 'fernanda.rebelatto@gmail.com');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (15148309,2,'Sommelier de Airbnb','Guilh_rm_', '360guilherme@gmail.com');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (927028057763995648,3,'✪ Felippe','FelippeRegazio', 'felippe.moraes@zoho.com');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (3429765093,4,'Paula Santana','psanrosa13', 'psanrosa13@gmail.com');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (10794662,5,'Maharaja of the Legacies','rponte', 'rponte@gmail.com');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (1034514325644431361,6,'Jessica','dev_jessi', 'jessi.cflx@gmail.com');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (12050562,7,'Roberta Arcoverde','rla4', 'roberta.arcoverde@gmail.com');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (62314491,8,'Gabriela','Gabrielathalita', 'gabrielathalitasv@gmail.com');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (172880784,9,'felipe.ts ⚡','felipefialho_', 'hi@felipefialho.com');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (18609547,10,'Juan Lopes','juanplopes', 'me@juanlopes.net');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (2164986296,11,'Rafael Dantas','RaffaelDantass', 'rafael.dantas.br@gmail.com');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (1026783544457273345,12,'iagor com i','Iagor51', 'iagorluchesi@gmail.com');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (109604376,13,'T̷o̷m̷o̷y̷o̷ ̷ F̷u̷r̷a̷c̷ã̷o̷','RafaelMansilha', 'rafael.mansilha@gmail.com');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (1040939562774790144,14,'Esdras Xavier','esdras_xavieer', 'esdrasxa@gmail.com');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (69734383,15,'belle-?.dev || twitch.tv/bellesamways','bellesamways', 'belesamways@gmail.com');
+# insert into User (id_str_twitter, participant_id, name, screen_name, email) VALUES (119897261,16,'Willian Justen','Willian_justen', 'willianjustenqui@gmail.com');
