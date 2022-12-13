2b28f90511eab294988475a731d394f162aaec4a
Leo Moreira Silva <leo.moreira@me.com>
2022-12-13 13:16:26 -0300

    remocao de key

--- Projects/TwitterDataMining/TwitterDataCollection/api_key.py
+++ Projects/TwitterDataMining/TwitterDataCollection/api_key.py
@@ -1,6 +1,6 @@
 key = {
-  "consumer_key": "nHAF4VfPXSFW4yBs8w2MKnNty",
-  "consumer_secret": "ZnrJTcRKVq9pqrnLDFkeTZBrZ0z53av8cYwcLIXFzVM2xyFZHD",
-  "access_token": "AAAAAAAAAAAAAAAAAAAAAPiIKwEAAAAA9WtZ8uEanLGM0HIGREA3wQNj9aU%3DG14tlXfigXCmX5uY3JR5t85Lgr1WYHcJmAQKXw8GO1QGQu80Dw",
+  "consumer_key": "",
+  "consumer_secret": "",
+  "access_token": "",
   "access_token_secret": ""
 }
